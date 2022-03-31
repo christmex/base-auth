21135429b3dd8252ba077c7f94511d2eeb5daf1f
christmex <christmex@yahoo.com>
2022-03-31 15:22:35 +0700

    Initial Commit

---
+++ src/BaseAuthServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Christmex\BaseAuth;
+
+use Illuminate\Support\ServiceProvider;
+
+class BaseAuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // include the routes
+        $this->loadRoutesFrom(__DIR__.'/routes.php');
+
+        //include the views
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'baseauth');
+    }
+}
---
+++ src/routes.php
@@ -0,0 +1,6 @@
+<?php
+
+Route::get('/baseauth', function() {
+    // echo 'Hello from the demo package!';
+    return view('baseauth::demo');
+});
